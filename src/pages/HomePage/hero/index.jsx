9fca934a00c45a2b0ad72d545001a02f142836a5
urtworkmail <usamarehmantarar.workmail@gmail.com>
2026-01-22 20:08:51 +0500

    Update index.jsx

--- src/pages/HomePage/hero/index.jsx
+++ src/pages/HomePage/hero/index.jsx
@@ -27,7 +27,7 @@ export default function Hero() {
         >
           <SectionTitle
             title="Connect Freight & Carriers with AI-Powered Precision"
-            paragraph="Join thousands of drivers and shippers who trust WESTERN EAGLE TRANSPORT INC for fast, secure, and profitable freight matching. Get started in minutes."
+            paragraph="Join thousands of drivers and shippers who trust AU Truckers for fast, secure, and profitable freight matching. Get started in minutes."
             center
             width="700px"
             mb="50px"
